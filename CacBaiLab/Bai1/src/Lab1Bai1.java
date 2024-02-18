ee3861a7fa12fab56eb822638c2c23d3c7e84e4d
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2024-02-18 18:35:32 +0700

    Lab1Bai1

---
+++ CacBaiLab/Bai1/src/Lab1Bai1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Lab1Bai1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+	    
+	    System.out.print("Họ và tên: ");  
+	    String hoTen = scanner.nextLine();
+	    
+	    System.out.print("Điểm TB: "); 
+	    double diemTB = scanner.nextDouble();
+	    
+		System.out.printf("%s %.2f điểm", hoTen, diemTB);
+	}
+
+}
