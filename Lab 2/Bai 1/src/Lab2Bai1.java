0322ec7cc5e51bcae7825b9faba9b8ecb348809c
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2024-02-18 19:34:45 +0700

    Lab 2 Bai 1

---
+++ Lab 2/Bai 1/src/Lab2Bai1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Lab2Bai1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Nhập hệ số a: ");  
+	    double a = scanner.nextDouble();
+	    
+	    System.out.print("Nhập hệ số b: ");  
+	    double b = scanner.nextDouble();
+	    
+	    if(a == 0){
+	    	if(b == 0){
+	    		System.out.println("Phương trình vô số nghiệm");
+	    	} else {
+	    		System.out.println("Phương trình vô nghiệm");
+	    	}
+	    } else {
+	    	double x = -b/a;
+	    	System.out.printf("Phương trình có nghiệm là : %.1f",x);
+	    }
+	}
+}
