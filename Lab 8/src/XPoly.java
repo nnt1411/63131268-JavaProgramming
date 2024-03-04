5f10bfdd190e91ffe4de0c726ba4d1e457fe86ca
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2024-03-04 19:07:13 +0700

    Lab 8 bai 1,2

--- Lab 8/src/XPoly.java
+++ Lab 8/src/XPoly.java
@@ -10,5 +10,24 @@ public class XPoly {
         }
         return total;
     }
-
+    
+    public static double min(double... x) {
+        double min = Double.MAX_VALUE; // Khởi tạo min với giá trị lớn nhất
+        for (double value : x) {
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+    
+    public static double max(double... x) {
+        double max = Double.MIN_VALUE; // Khởi tạo max với giá trị nhỏ nhất
+        for (double value : x) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
 }
